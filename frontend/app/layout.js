cafb05b2862fc36e46cb001ba5fcff5fe669cfc1
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-01-09 15:23:28 +0100

    update network chain

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -7,11 +7,11 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { hardhat, sepolia, polygonMumbai } from 'wagmi/chains';
+import { hardhat, sepolia } from 'wagmi/chains';
 
 const { chains, publicClient } = configureChains(
-  process.env.NEXT_PUBLIC_ENV === 'dev' ? [hardhat] : [sepolia, polygonMumbai],
-  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }), publicProvider()]
+    [process.env.NEXT_PUBLIC_ENV === 'dev' ? hardhat : sepolia],
+    [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
