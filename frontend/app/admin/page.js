4eb91b185bcf2f8f354e9b959e60729357ea42ad
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-01-10 17:02:08 +0100

    Update page.js

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -16,7 +16,6 @@ import { ContractFunctionExecutionError, formatEther } from 'viem'
 
 const page = () => {
     /* State */
-    // Viem
     const viemPublicClient = usePublicClient();
     const [balanceSmartContract, setBalanceSmartContract] =  useState('')
     const { address, isConnected } = useAccount()
@@ -26,17 +25,19 @@ const page = () => {
     const [successTransaction, setSuccessTransaction] =  useState(false)
     const [errorTransaction, setErrorTransaction] =  useState('')
     const [account, setAccount] =  useState('')
+    const [newBaseURI, setNewBaseURI] =  useState('')
     const [baseURI, setBaseURI] =  useState('')
     const [quantity, setQuantity] =  useState(0)
+    const [selectedDate, setSelectedDate] = useState(new Date());
     
-    const giftTokens = async (account, quantity) => {
+    const giftTokens = async (_account, _quantity) => {
         if (isConnected) {
             try {
               const { request } = await prepareWriteContract({
                   address: contractAddressRoomlab,
                   abi: contractAbiRoomlab,
                   functionName: "gift",
-                  args: [account, quantity]
+                  args: [_account, _quantity]
               });
               const { hash } = await writeContract(request);
               setPendingTransaction(true)
@@ -64,7 +65,46 @@ const page = () => {
                 setErrorTransaction("An error occured")
               }
             }
-          }
+        }
+    }
+
+    const setUri = async (_baseUri) => {
+        if (isConnected && isOwner) {
+            try {
+              const { request } = await prepareWriteContract({
+                  address: contractAddressRoomlab,
+                  abi: contractAbiRoomlab,
+                  functionName: "setBaseURI",
+                  args: [ _baseUri ]
+              });
+              const { hash } = await writeContract(request);
+              setPendingTransaction(true)
+              await waitForTransaction({hash: hash})
+              setPendingTransaction(false)
+              setSuccessTransaction(true)
+              return hash;
+            } catch (err) {
+              console.log(err)
+              if( err instanceof ContractFunctionExecutionError) {
+                if("CallExecutionError" === err.cause.name) {
+                    setErrorTransaction(err.cause.shortMessage)
+                } else {
+                    setErrorTransaction("An error occured");
+                }
+              } else {
+                setErrorTransaction("An error occured")
+              }
+            }
+        }
+    }
+
+    const getBaseUri = async () => {
+        const baseUri = await readContract({
+            address: contractAddressRoomlab,
+            abi: contractAbiRoomlab,
+            functionName: 'getBaseURI',
+        });
+        setBaseURI(baseUri)
     }
 
     const isUserTheOwner = async () => {
@@ -93,17 +133,35 @@ const page = () => {
 
     const handleSubmitFormBaseURI = (e) => {
         e.preventDefault();
+        setUri(newBaseURI)
     }
 
+    const handleDateChange = (e) => {
+        const { name, value } = e.target;
+        console.log(selectedDate)
+        const newDate = { ...selectedDate, [name]: parseInt(value, 10) };
+        console.log(newDate)
+    
+        //setSelectedDate(newDate);
+      };
+
+    const handleSubmitDate = (e) => {
+        e.preventDefault();
+        const timestamp = selectedDate.getTime();
+        console.log('Selected Timestamp:', timestamp);
+        // Ajoutez ici le code pour traiter le timestamp
+    }
 
     useEffect(() => {
         isUserTheOwner()
         getBalanceSmartContract()
+        getBaseUri()
     }, [isConnected, address])
 
     useEffect(() => {
         isUserTheOwner()
         getBalanceSmartContract()
+        getBaseUri()
     }, [])
 
     return (
@@ -115,15 +173,13 @@ const page = () => {
                             <h1 className="text-2xl clay-primary font-serif font-bold" style={{ fontFamily: "'Montserrat', sans-serif" }}><Link href="/">RoomLab</Link></h1>
                             <nav>
                                 <ul className="flex items-center space-x-4">
-                                    {balanceSmartContract && (
-                                        <li>Balance smart contract : {balanceSmartContract} ETH</li>)}
                                     <li><ConnectButton /></li>
                                 </ul>
                             </nav>
                         </div>
                     </header>
                     <main className="container mx-auto py-6 px-4 flex flex-wrap justify-center">
-                        <form onSubmit={handleSubmitFormGift}  className="max-w-[500px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
+                        <form onSubmit={handleSubmitFormGift}  className="max-w-[600px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
                             <div className="mb-4">
                                 <label htmlFor="address" className="block text-gray-600 font-semibold mb-2">Adresse EVM :</label>
                                 <input
@@ -152,38 +208,124 @@ const page = () => {
                             <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition duration-300">Offrir des NFTs</button>
                         </form>
 
-                        <form onSubmit={handleSubmitFormBaseURI}  className="max-w-[500px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
+                        <form onSubmit={handleSubmitFormBaseURI}  className="max-w-[600px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
                             <div className="mb-4">
                                 <label htmlFor="baseUri" className="block text-gray-600 font-semibold mb-2">Base URI :</label>
                                 <input
                                 type="text"
                                 id="baseUri"
-                                value={baseURI}
-                                onChange={(e) => setBaseURI(e.target.value)}
+                                value={newBaseURI}
+                                onChange={(e) => setNewBaseURI(e.target.value)}
                                 className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                                 required
                                 />
                             </div>
                             <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition duration-300">Définir une nouvelle base URI</button>
+                            <div className="mt-6 mb-4 p-2">
+                                <p className="">Base URI : <br/>{baseURI}</p>
+                            </div>
                         </form>
 
-                        <form className="max-w-[500px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
-                            <div className="mb-4">
-                                <label htmlFor="saleStartTime" className="block text-gray-600 font-semibold mb-2">Date de vente ouverte :</label>
-                                <input
-                                type="text"
-                                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                                required
-                                />
+                        <form onSubmit={handleSubmitDate} className="max-w-[600px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
+                            <div className="mb-4 flex flex-wrap">
+                                <label className="w-full md:w-1/5" htmlFor="day">
+                                    Jour:
+                                    <select
+                                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                        name="day"
+                                        value={selectedDate.getDate()}
+                                        onChange={handleDateChange}
+                                        required
+                                    >
+                                        {Array.from({ length: 31 }, (_, index) => index + 1).map((day) => (
+                                        <option key={day} value={day}>
+                                            {day}
+                                        </option>
+                                        ))}
+                                    </select>
+                                </label>
+                                <label className="w-full md:w-1/5" htmlFor="month">
+                                    Mois:
+                                    <select
+                                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                        name="month"
+                                        value={selectedDate.getMonth() + 1}
+                                        onChange={handleDateChange}
+                                        required
+                                    >
+                                        {Array.from({ length: 12 }, (_, index) => index + 1).map((month) => (
+                                        <option key={month} value={month}>
+                                            {month}
+                                        </option>
+                                        ))}
+                                    </select>
+                                </label>
+                                <label className="w-full md:w-1/5" htmlFor="year">
+                                    Année:
+                                    <select
+                                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                        name="year"
+                                        value={selectedDate.getFullYear()}
+                                        onChange={handleDateChange}
+                                        required
+                                    >
+                                        {Array.from({ length: 100 }, (_, index) => selectedDate.getFullYear() + index).map((year) => (
+                                        <option key={year} value={year}>
+                                            {year}
+                                        </option>
+                                        ))}
+                                    </select>
+                                </label>
+                                <label className="w-full md:w-1/5" htmlFor="hours">
+                                    Heure:
+                                    <select
+                                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                        name="hours"
+                                        value={selectedDate.getHours()}
+                                        onChange={handleDateChange}
+                                        required
+                                    >
+                                        {Array.from({ length: 24 }, (_, index) => index).map((hour) => (
+                                        <option key={hour} value={hour}>
+                                            {hour}
+                                        </option>
+                                        ))}
+                                    </select>
+                                </label>
+                                <label className="w-full md:w-1/5" htmlFor="minutes">
+                                    Minutes:
+                                    <select
+                                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                        name="minutes"
+                                        value={selectedDate.getMinutes()}
+                                        onChange={handleDateChange}
+                                        required
+                                    >
+                                        {Array.from({ length: 60 }, (_, index) => index).map((minute) => (
+                                        <option key={minute} value={minute}>
+                                            {minute}
+                                        </option>
+                                        ))}
+                                    </select>
+                                </label>
                             </div>
                             <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition duration-300">Définir une date de vente ouverte</button>
                         </form>
 
-                        <form className="max-w-[500px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
+
+
+                        <form className="max-w-[600px] w-full mx-4 mt-8 p-4 bg-white shadow-md rounded-md">
                             <div className="mb-4">
                                 <label htmlFor="withdraw" className="block text-gray-600 font-semibold mb-2">Transfer :</label>
                             </div>
                             <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition duration-300">Récupérer les fonds</button>
+                            {balanceSmartContract && (
+                                <>
+                                    <div className="mt-6 mb-4 p-2">
+                                        <p>Balance smart contract : {balanceSmartContract} ETH</p>
+                                    </div>
+                                </>
+                            )}
                         </form>
                     </main>
                 </>
